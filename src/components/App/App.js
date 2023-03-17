9c4d915986b585223bda38350c74286dd7ffb9c4
Iryna Artymuk <artimyk@ukr.net>
2023-03-17 06:57:01 +0200

    colorpiker

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <>
-        <ColorPicker options={data} />
+        {/* <ColorPicker options={data} /> */}
         <ColorPickerHooks options={data} />
         <SignupForm />
       </>
--- src/components/ColorPikerHooks/ColorPikerHooks.jsx
+++ src/components/ColorPikerHooks/ColorPikerHooks.jsx
@@ -1,39 +1,43 @@
 import './ColorPickerHooks.css';
-
+import { useState } from 'react';
 import classNames from 'classnames/bind';
-function makeClassList(index) {
-  return classNames('button', {
-    // activeButton: index === this.state.activeButtonIndex,
-  });
-}
-function setActiveIndexToState(iidex) {}
+
 export default function ColorPickerHooks({ options }) {
-  // const { activeButtonIndex } = this.state;
-  // const activeButton = options[activeButtonIndex];
+  const [activeButtonIndex, SetActiveIndex] = useState(0);
 
-  // const activeBackground = activeButton.color;
+  const activeButtonObject = options[activeButtonIndex];
 
+  const activeBackground = activeButtonObject.color;
+  const setActiveIndex = newIndex => {
+    SetActiveIndex(newIndex);
+  };
+  const makeClassList = index => {
+    return classNames('button', {
+      ' activeButton': index === activeButtonIndex,
+    });
+  };
   return (
     <div
       className="colorContainer"
-      style={
-        {
-          // backgroundColor: activeBackground,
-        }
-      }
+      style={{
+        backgroundColor: activeBackground,
+      }}
     >
       <h2 className="title"> Color Picker Hooks</h2>
       <div className="colorBox">
-        {options.map(({ lable, color }, index) => (
-          <button
-            key={lable}
-            className={makeClassList(index)}
-            style={{
-              backgroundColor: color,
-            }}
-            onClick={() => setActiveIndexToState(index)}
-          ></button>
-        ))}
+        {options.map(({ lable, color }, index, array) => {
+          // console.log(array); // вихідний масив
+          return (
+            <button
+              key={lable}
+              className={makeClassList(index)}
+              style={{
+                backgroundColor: color,
+              }}
+              onClick={() => setActiveIndex(index)}
+            ></button>
+          );
+        })}
       </div>
     </div>
   );
